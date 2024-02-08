4c775db496ad37bc10ebbc305b4370c8b8078414
Nabid-Rabbi <144992834+Nabid-Rabbi@users.noreply.github.com>
2024-02-08 22:54:51 +0600

    Add files via upload

---
+++ HTML-CSS-JS/slideshow-demo/script.js
@@ -0,0 +1,18 @@
+const slideShowElements = document.querySelectorAll(".slideshow-element");
+
+
+let countElements = 1;
+setInterval(() => {
+    countElements++;
+   let currentElement = document.querySelector(".current");
+
+   currentElement.classList.remove("current");
+
+   if(countElements>slideShowElements.length){
+    slideShowElements[0].classList.add("current");
+   }else{
+    currentElement.nextElementSibling.classList.add("current");
+   }
+
+   
+}, 2000);
